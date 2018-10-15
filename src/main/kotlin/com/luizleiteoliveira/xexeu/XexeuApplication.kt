c526304cfe49f1d9a3eb632ffab98b3f5bc7948f
luizleite-hotmart <luiz.leite@hotmart.com>
2018-10-15 19:09:30 -0300

    start_from_project

---
+++ src/main/kotlin/com/luizleiteoliveira/xexeu/XexeuApplication.kt
@@ -0,0 +1,11 @@
+package com.luizleiteoliveira.xexeu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XexeuApplication
+
+fun main(args: Array<String>) {
+    runApplication<XexeuApplication>(*args)
+}
---
+++ src/test/kotlin/com/luizleiteoliveira/xexeu/XexeuApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.luizleiteoliveira.xexeu
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class XexeuApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
