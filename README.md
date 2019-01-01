7a5859c74b4e19a2158ada25557faad154157164
luizleite-hotmart <luiz.leite@hotmart.com>
2019-01-01 15:26:27 -0200

    begin from project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Xexeu
+
+## This project will be a download center for amazon s3
+For this happen this will be the TODO list fell free to help with that.
+
+- [x] TODO LIST :stuck_out_tongue_winking_eye: 
---
+++ src/test/java/com/luizleite/xexeu/xexeu/XexeuApplicationTests.java
@@ -0,0 +1,17 @@
+package com.luizleite.xexeu.xexeu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XexeuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
