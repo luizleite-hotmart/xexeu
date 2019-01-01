a2f20f454cba37ab987f290472faecb482492af7
luizleite-hotmart <luiz.leite@hotmart.com>
2019-01-01 16:15:47 -0200

    bucket controller

--- src/main/java/com/luizleite/xexeu/xexeu/controller/BucketController.kt
+++ src/main/java/com/luizleite/xexeu/xexeu/controller/BucketController.kt
@@ -1,3 +1,17 @@
 package com.luizleite.xexeu.xexeu.controller
 
-//@RestControl
+import org.apache.catalina.User
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("rest/v1/bucket")
+class BucketController {
+
+    @GetMapping
+    fun getUserBucket(user: User) : ResponseEntity<*> {
+        return ResponseEntity.ok("start is ok")
+    }
+}
