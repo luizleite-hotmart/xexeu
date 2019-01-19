f24d890a3efa4518086f52e9b4821f75745bb2b5
luizleite-hotmart <luiz.leite@hotmart.com>
2019-01-19 11:28:01 -0200

    example from properties

--- src/main/java/com/luizleite/xexeu/xexeu/controller/BucketController.kt
+++ src/main/java/com/luizleite/xexeu/xexeu/controller/BucketController.kt
@@ -1,6 +1,8 @@
 package com.luizleite.xexeu.xexeu.controller
 
 import com.luizleite.xexeu.xexeu.entity.User
+import com.luizleite.xexeu.xexeu.services.BucketService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("rest/v1/bucket")
-class BucketController {
+class BucketController @Autowired constructor(private val bucketService: BucketService){
+
 
     @GetMapping
     fun getUserBucket(user: User) : ResponseEntity<*> {
+        val list = bucketService.findAllFilesFromUser(user.slug,user.name)
         return ResponseEntity.ok("start is ok ${user.slug}")
     }
 }
