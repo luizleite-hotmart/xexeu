1d6db5e867a77e2c1bcf47ea03e7a91f29a2689a
luizleite-hotmart <luiz.leite@hotmart.com>
2019-01-19 09:31:45 -0200

    create test for UserService class

---
+++ src/test/java/com/luizleite/xexeu/xexeu/services/UserServiceTest.kt
@@ -0,0 +1,16 @@
+package com.luizleite.xexeu.xexeu.services
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class UserServiceTest {
+    @Test
+    fun verifyIfUserExists() {
+    }
+
+    @Test
+    fun createUser() {
+    }
+
+}
