edb4a211ff7acc908a4be2c5cee7eda8f4d98e33
luizleite-hotmart <luiz.leite@hotmart.com>
2019-01-19 09:22:55 -0200

    crete a service for user

--- src/main/java/com/luizleite/xexeu/xexeu/services/UserService.kt
+++ src/main/java/com/luizleite/xexeu/xexeu/services/UserService.kt
@@ -1,6 +1,21 @@
 package com.luizleite.xexeu.xexeu.services
 
-import org.springframework.stereotype.Service
+import com.luizleite.xexeu.xexeu.entity.User
+import com.luizleite.xexeu.xexeu.repositorys.UserRepository
 
+import org.springframework.stereotype.Service
 @Service
-class UserService
+class UserService {
+
+    val userRepository:UserRepository
+
+    constructor(userRepository: UserRepository) {
+        this.userRepository = userRepository
+    }
+
+    fun verifyIfUserExists(slug: String): Boolean{
+        val user = userRepository.findUserBySlug(slug)
+        return user == null
+    }
+
+}
