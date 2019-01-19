9265874b7570fd792f5436b1a84489e3e766bf7a
luizleite-hotmart <luiz.leite@hotmart.com>
2019-01-19 09:15:47 -0200

    create crud repository for user

--- src/main/java/com/luizleite/xexeu/xexeu/entity/User.kt
+++ src/main/java/com/luizleite/xexeu/xexeu/entity/User.kt
@@ -1,3 +1,6 @@
 package com.luizleite.xexeu.xexeu.entity
 
+import javax.persistence.Entity
+
+@Entity
 data class User(var slug:String, var name:String?)
--- src/main/java/com/luizleite/xexeu/xexeu/repositorys/UserRepository.kt
+++ src/main/java/com/luizleite/xexeu/xexeu/repositorys/UserRepository.kt
@@ -1,1 +1,6 @@
 package com.luizleite.xexeu.xexeu.repositorys
+
+import com.luizleite.xexeu.xexeu.entity.User
+import org.springframework.data.repository.CrudRepository
+
+interface UserRepository:CrudRepository<User,String>
